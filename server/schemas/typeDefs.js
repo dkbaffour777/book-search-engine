1e3995d48a66ecf4a1dc6c06adcac8f31e6b829f
Daniel <dkbaffour777@gmail.com>
2021-07-30 20:41:51 -0700

    setup typeDefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,47 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks : [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
-    helloWorld: String
+    me: User
+  }
+
+  input saveBookContent {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }  
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(content: saveBookContent!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
