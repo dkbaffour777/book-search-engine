71319194f65c9b39cc0f2785223fd66320001b95
Daniel <dkbaffour777@gmail.com>
2021-08-05 18:15:24 -0700

    add build path

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -19,6 +20,9 @@ const {authMiddleware} = require('./utils/auth');
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
   
   await new Promise(resolve => 
